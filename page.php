40039ec8f05dd35c5fc4c0f3882712a8219830fb
Thomas McMahon <tmcmahon@nerdery.com>
2020-02-02 19:14:57 -0600

    remoiving old custom code from the page template

--- page.php
+++ page.php
@@ -2,30 +2,35 @@
 
 <section class="content" role="main">
 <a id="mainContent" href="" role="heading" tabindex="-1" class="isVisuallyHidden" aria-level="2">Main Content</a>
-	
+
 	<div class="pad group">
-		
+
 		<?php while ( have_posts() ): the_post(); ?>
-		
+
 			<article <?php post_class('group'); ?>>
-				
+
 				<?php get_template_part('inc/page-image'); ?>
-				
-				<h1 class="page-title-single"><?php echo alx_page_title(); ?></h1>
-				
+
+				<h1 class="page-title-single"><?php echo get_the_title(); ?></h1>
+
 				<div class="entry themeform">
 					<?php the_content(); ?>
 					<div class="clear"></div>
 				</div><!--/.entry-->
-				
+
 			</article>
-			
-			<?php if ( ot_get_option('page-comments') == 'on' ) { comments_template('/comments.php',true); } ?>
-			
+
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template('/comments.php', true);
+			}
+			?>
+
 		<?php endwhile; ?>
-		
+
 	</div><!--/.pad-->
-	
+
 </section><!--/.content-->
 
 <?php get_sidebar(); ?>
