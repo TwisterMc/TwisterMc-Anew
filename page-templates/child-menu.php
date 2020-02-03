552e1e08ff039bdd34e66c855884ff8b0130ee84
Thomas McMahon <tmcmahon@nerdery.com>
2020-02-02 19:33:43 -0600

    WP excerpts

--- page-templates/child-menu.php
+++ page-templates/child-menu.php
@@ -24,8 +24,12 @@ Template Name: Child Menu
 
 			</article>
 
-			<?php if ( ot_get_option('page-comments') == 'on' ) { comments_template('/comments.php',true); } ?>
-
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template('/comments.php', true);
+			}
+			?>
 		<?php endwhile; ?>
 
 	</div><!--/.pad-->
