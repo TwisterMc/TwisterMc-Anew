3451c58697bb3e3d71a603b34cf63254d6c55cf7
Thomas McMahon <tmcmahon@nerdery.com>
2020-02-02 18:52:50 -0600

    removing option tree from footer

--- footer.php
+++ footer.php
@@ -1,59 +1,59 @@
 		</div><!--/.main-->
 	</div><!--/.container-inner-->
-	
-	<footer id="footer" role="contentinfo">	
-		<?php // footer widgets
-			$total = 4;
-			if ( ot_get_option( 'footer-widgets' ) != '' ) {
-				
-				$total = ot_get_option( 'footer-widgets' );
-				if( $total == 1) $class = 'one-full';
-				if( $total == 2) $class = 'one-half';
-				if( $total == 3) $class = 'one-third';
-				if( $total == 4) $class = 'one-fourth';
-				}
 
-				if ( ( is_active_sidebar( 'footer-1' ) ||
-					   is_active_sidebar( 'footer-2' ) ||
-					   is_active_sidebar( 'footer-3' ) ||
-					   is_active_sidebar( 'footer-4' ) ) && $total > 0 ) 
-		{ ?>		
+	<footer id="footer" role="contentinfo">
 		<section id="footer-widgets" class="container">
 			<div class="pad group">
-				<?php $i = 0; while ( $i < $total ) { $i++; ?>
-					<?php if ( is_active_sidebar( 'footer-' . $i ) ) { ?>
-		
-				<div class="footer-widget-<?php echo $i; ?> grid <?php echo $class; ?> <?php if ( $i == $total ) { echo 'last'; } ?>">
-					<?php dynamic_sidebar( 'footer-' . $i ); ?>
+					<?php
+					if ( is_active_sidebar( 'footer-1' ) ) :
+						echo '<div class="grid one-fourth">';
+						dynamic_sidebar( 'footer-1' );
+						echo '</div>';
+					endif;
+					if ( is_active_sidebar( 'footer-2' ) ) :
+						echo '<div class="grid one-fourth">';
+						dynamic_sidebar( 'footer-2' );
+						echo '</div>';
+					endif;
+					if ( is_active_sidebar( 'footer-3' ) ) :
+						echo '<div class="grid one-fourth">';
+						dynamic_sidebar( 'footer-3' );
+						echo '</div>';
+					endif;
+					if ( is_active_sidebar( 'footer-4' ) ) :
+						echo '<div class="grid one-fourth">';
+						dynamic_sidebar( 'footer-4' );
+						echo '</div>';
+					endif;
+				?>
 				</div>
-		
-					<?php } ?>
-				<?php } ?>
 			</div><!--/.pad-->
-		</section><!--/#footer-widgets-->	
-		<?php } ?>
-		
+		</section><!--/#footer-widgets-->
+
 		<section id="footer-bottom">
 			<div class="container">
-			
+
 				<a id="back-to-top" href="#"><span class="isVisuallyHidden">Back to Top</span><i class="fa fa-angle-up"></i></a>
-				
-				<div class="pad group">				
+
+				<div class="pad group">
 					<div class="grid one-half">
-						
+
 						<div id="copyright">
 							<p><?php bloginfo(); ?> &copy; <?php echo date( 'Y' ); ?>. <?php _e('All Rights Reserved.','anew'); ?></p>
 						</div><!--/#copyright-->
-						
-					</div>		
-					<div class="grid one-half last">	
-						<?php alx_social_links() ; ?>
-					</div>		
+
+					</div>
+					<div class="grid one-half last">
+						<ul class="social-links">
+							<li><a rel="nofollow" class="social-tooltip" title="Twitter" href="http://twitter.com/twistermc" target="_blank"><i class="fa fa-twitter"></i></a></li>
+							<li><a rel="nofollow" class="social-tooltip" title="RSS Feed" href="https://www.twistermc.com/feed/" target="_blank"><i class="fa fa-rss"></i></a></li>
+						</ul>
+					</div>
 				</div><!--/.pad-->
-				
+
 			</div><!--/.container-->
 		</section><!--/#footer-bottom-->
-		
+
 	</footer><!--/#footer-->
 
 </div><!--/#wrapper-->
