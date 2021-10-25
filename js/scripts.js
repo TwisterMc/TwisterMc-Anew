20ba5faa7eb74020e5f9369eb347817809446a98
Thomas McMahon <tmcmahon@nerdery.com>
2021-10-25 09:12:53 -0500

    Cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 /*
 	scripts.js
-	
+
 	License: GNU General Public License v3.0
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
-	
+
 	Copyright: (c) 2013 Alexander "Alx" Agnarson, http://alxmedia.se
 */
 
@@ -18,16 +18,16 @@ jQuery(document).ready(function($) {
                 $('.search-expand input').focus();
             }, 300);
 	});
-	
+
 /*  Scroll to top
 /* ------------------------------------ */
 	$('a#back-to-top').click(function() {
 		$('html, body').animate({scrollTop:0},'slow');
 		return false;
 	});
-	
+
 /*  Tabs widget
-/* ------------------------------------ */	
+/* ------------------------------------ */
 	(function() {
 		var $tabsNav       = $('.alx-tabs-nav'),
 			$tabsNavLis    = $tabsNav.children('li'),
@@ -45,16 +45,16 @@ jQuery(document).ready(function($) {
 
 			$this.siblings().removeClass('active').end()
 			.addClass('active');
-			
+
 			$this.parent().next().children('.alx-tab').stop(true,true).hide()
 			.siblings( $this.find('a').attr('href') ).fadeIn();
 			e.preventDefault();
 		}).children( window.location.hash ? 'a[href=' + window.location.hash + ']' : 'a:first' ).trigger('click');
 
 	})();
-	
+
 /*  Comments / pingbacks tabs
-/* ------------------------------------ */	
+/* ------------------------------------ */
     $(".comment-tabs li").click(function() {
         $(".comment-tabs li").removeClass('active');
         $(this).addClass("active");
@@ -64,41 +64,37 @@ jQuery(document).ready(function($) {
         return false;
     });
 
-/*  Table odd row class
-/* ------------------------------------ */
-	$('table tr:odd').addClass('alt');
-
 /*  Sidebar collapse
 /* ------------------------------------ */
 	$('body').addClass('s1-collapse');
 	$('body').addClass('s2-collapse');
-	
+
 	$('.s1 .sidebar-toggle').click(function(){
 		$('body').toggleClass('s1-collapse').toggleClass('s1-expand');
-		if ($('body').is('.s2-expand')) { 
+		if ($('body').is('.s2-expand')) {
 			$('body').toggleClass('s2-expand').toggleClass('s2-collapse');
 		}
 	});
 	$('.s2 .sidebar-toggle').click(function(){
 		$('body').toggleClass('s2-collapse').toggleClass('s2-expand');
-		if ($('body').is('.s1-expand')) { 
+		if ($('body').is('.s1-expand')) {
 			$('body').toggleClass('s1-expand').toggleClass('s1-collapse');
 		}
 	});
 
 
-	
+
 /*  Mobile menu smooth toggle height
-/* ------------------------------------ */	
+/* ------------------------------------ */
 	$('.nav-toggle').on('click', function() {
 		slide($('.nav-wrap .nav', $(this).parent()));
 	});
-	 
+
 	function slide(content) {
 		var wrapper = content.parent();
 		var contentHeight = content.outerHeight(true);
 		var wrapperHeight = wrapper.height();
-	 
+
 		wrapper.toggleClass('expand');
 		if (wrapper.hasClass('expand')) {
 		setTimeout(function() {
@@ -113,12 +109,12 @@ jQuery(document).ready(function($) {
 			}, 10);
 		}, 10);
 	}
-	 
+
 	wrapper.one('transitionEnd webkitTransitionEnd transitionend oTransitionEnd msTransitionEnd', function() {
 		if(wrapper.hasClass('open')) {
 			wrapper.removeClass('transition').css('height', 'auto');
 		}
 	});
 	}
-	
+
 });
