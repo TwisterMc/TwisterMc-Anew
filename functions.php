5e5108391afe9ae4b49675e0148572638f07aad4
Thomas McMahon <tmcmahon@nerdery.com>
2022-01-28 16:11:10 -0600

    Removing Google Photos

--- functions.php
+++ functions.php
@@ -417,4 +417,21 @@ function jetpackme_related_posts_time_restriction( $date_range ) {
 	return $date_range;
 
 }
+
 add_filter( 'jetpack_relatedposts_filter_date_range', 'jetpackme_related_posts_time_restriction' );
+
+/**
+ * Remove Jetpack's External Media feature.
+ */
+add_action(
+	'enqueue_block_editor_assets',
+	function () {
+		$disable_external_media = <<<JS
+document.addEventListener( 'DOMContentLoaded', function() {
+    wp.hooks.removeFilter( 'blocks.registerBlockType', 'external-media/individual-blocks' );
+    wp.hooks.removeFilter( 'editor.MediaUpload', 'external-media/replace-media-upload' );
+} );
+JS;
+		wp_add_inline_script( 'jetpack-blocks-editor', $disable_external_media );
+	}
+);
